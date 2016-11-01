202f0e252ffef5dfc27aa89122d37d734721bc52
Scott Hayward <scotthayward@gmail.com>
2016-11-01 13:16:28 -0400

    resume at receiving form data in lesson 3

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     user = {'nickname': 'Scott'}  # fake user
     posts = [  # fake array of posts
@@ -24,3 +26,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
+
